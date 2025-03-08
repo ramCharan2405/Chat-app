f214f16c772641bdd1a6be183b2f6ff28a2e22cc
Ram Charan <ramcharanchary6300@gmail.com>
2025-03-08 14:45:12 +0530

    Initial commit: add project structure with backend and frontend

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import{connectDB} from './lib/db.js';
+dotenv.config(); // Load environment variables first
+
+import authRoutes from './routes/auth.route.js';
+
+const app = express();
+const PORT = process.env.PORT || 5000; 
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+    connectDB();
+});
