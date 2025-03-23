c9227bec4f8d051c6949266cac0b1e12469aea47
Ram Charan <ramcharanchary6300@gmail.com>
2025-03-23 12:55:42 +0530

    Implemented Socket.io

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,6 +1,7 @@
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 import cloudinary  from "../lib/cloudinary.js";
+import { get } from "mongoose";
 
 export const getUsersForSidebar = async (req, res) => {
     try {
@@ -49,6 +50,11 @@ export const sendMessage = async (req, res) => {
         });
         await newMessage.save();
         //realtime functionality
+        const receiverSocketId =getReceiverSocketId(receiverId);
+        if(receiverSocketId){
+            io.to(receiverSocketId).emit("newMessage",newMessage);
+        }
+
         res.status(200).json(newMessage);
     } catch (error) {
         console.log("Error in sendMessage controller", error.message);
--- backend/src/index.js
+++ backend/src/index.js
@@ -8,8 +8,9 @@ dotenv.config(); // Load environment variables first
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
+import { io, app, server } from './lib/socket.js';
 
-const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 // Increase the JSON and URL-encoded payload limits to 50mb
@@ -25,7 +26,7 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`);
     connectDB();
 });
