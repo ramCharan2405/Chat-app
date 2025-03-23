1dc11d9013ebf194735d50c7318f445f017a810b
Ram Charan <ramcharanchary6300@gmail.com>
2025-03-23 11:28:00 +0530

    Completely Updated Chat Container

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -4,6 +4,7 @@ import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
 import { useAuthStore } from "../store/useAuthStore";
+import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = () => {
   const { messages, getMessages, isMessagesLoading, selectedUser } =
@@ -38,8 +39,8 @@ const ChatContainer = () => {
                 <img
                   src={
                     message.senderId === authUser._id
-                      ? authUser.profilePic || "avatar.png"
-                      : selectedUser.profilePic || "avatar.png"
+                      ? authUser.profilePic || "image.png"
+                      : selectedUser.profilePic || "image.png"
                   }
                   alt="profilePic"
                 />
@@ -47,10 +48,10 @@ const ChatContainer = () => {
             </div>
             <div className="chat-header mb-1">
               <time className="text-xs opacity-50 ml-1">
-                {message.createdAt}
+                {formatMessageTime(message.createdAt)}
               </time>
             </div>
-            <div className="chat-bubble flex">
+            <div className="chat-bubble flex flex-col ">
               {message.image && (
                 <img src={message.image} alt="Attachement"
                 className="sm:max-w-[200px] rounded-mb mb-2" />
